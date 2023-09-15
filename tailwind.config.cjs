a9a3ce8c5c68abd92e88826e6c15def36a8460be
MrHarvyson <josecmoreno3d@gmail.com>
2023-09-15 12:30:21 +0200

    .

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,cjs,mjs}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'azul': '#0bceaf',
+			  },
+		},
 		screens: {
 			'sm': '640px', // Cambia el valor a '640px'
 			'md': '768px',
