af360e7b62082ef18eb9e5c3bf87cdaad0e8fc85
José Carlos Moreno Martín <josecmoreno3d@gmail.com>
2024-05-09 01:51:46 +0200

    configuracion ruta en astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://AstroCV.github.io",
-  integrations: [tailwind()]
+  base: "/public",
+  integrations: [tailwind()],
 });
 //
