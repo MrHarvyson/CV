435fefaa1530ef9cdd6bf7c86d77929eb030a0ce
José Carlos Moreno Martín <josecmoreno3d@gmail.com>
2024-05-09 02:32:22 +0200

    nwe

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://AstroCV.github.io",
-  base: "/AstroCV.github.io",
   integrations: [tailwind()],
 })
