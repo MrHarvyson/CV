d556be63a6cbc86ad64be400ffb0c304e74b2e3d
José Carlos Moreno Martín <josecmoreno3d@gmail.com>
2024-05-10 00:49:14 +0200

    act

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mrharvyson.github.io',
+  site: 'https://Astro.github.io',
   integrations: [tailwind()],
 });
