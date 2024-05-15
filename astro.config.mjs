4591f664544d974d4ca55e085fdf2f2f70c179a8
José Carlos Moreno Martín <josecmoreno3d@gmail.com>
2024-05-15 23:47:45 +0200

    eliminacion cardA cardB

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mrharvyson.github.io',
-  base: 'Astro',
+  base: '',
   integrations: [tailwind()],
 });
