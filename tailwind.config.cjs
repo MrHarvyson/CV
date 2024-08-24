843c895f712fabac9ab293e3040d2a7af5255e08
DESKTOP-KKCS8B0\jose <josecmoreno3d@gmail.com>
2024-08-24 19:58:52 +0200

    Mejora cambio foto color blanco cards

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,17 +5,18 @@ module.exports = {
 		extend: {
 			colors: {
 				'azul': '#0bceaf',
-				'fondo':'#2c2d30',
-				'amarillo':'#fbdd62',
-				'rojo':'#ff5380',
-			  },
+				'fondo': '#2c2d30',
+				'amarillo': '#fbdd62',
+				'rojo': '#ff5380',
+				'texto_simple': 'rgba(255, 255, 255, 0.3)'
+			},
 		},
 		screens: {
 			'sm': '640px', // Cambia el valor a '640px'
 			'md': '768px',
 			'lg': '1024px',
 			'xl': '1280px',
-		  },
+		},
 	},
 	plugins: [],
 }
