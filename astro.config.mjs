054e1807d73d48ef7e32dc776c4da86bc5594426
José Carlos Moreno Martín <josecmoreno3d@gmail.com>
2024-12-09 21:34:42 +0100

    feat: cambios para vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
-
+import vercel from '@astrojs/vercel/serverless';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
@@ -7,4 +7,6 @@ export default defineConfig({
   site: 'https://mrharvyson.github.io',
   base: 'Astro',
   integrations: [tailwind()],
+  output: 'server',
+  adapter: vercel(),
 });
